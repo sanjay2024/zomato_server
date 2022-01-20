e7989a7b8668a6d60a4180ab2d5dea7fac6883f3
sanjay2024 <sanjayhari2003@gmail.com>
2022-01-20 22:53:36 +0530

    little change

--- src/config/google.config.js
+++ src/config/google.config.js
@@ -10,7 +10,7 @@ export default (passport) => {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         callbackURL:
-          "https://zomato--server.herokuapp.com/auth/google/callback",
+          "https://zomato---server.herokuapp.com/auth/google/callback",
       },
       async (accessToken, refreshToken, profile, done) => {
         // create a new user object
